ae4705af689ab8e46e32bb311b3470eb09adf38d
Nitin-Patare <166031025+Nitin-Patare@users.noreply.github.com>
2024-04-04 19:57:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ provider "google" {
   project     = "prj-21-417004"
   region      = "us-central1"  # Set your desired region
 }
+
+resource "google_compute_instance" "example" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "us-central1-b"
+  allow_stopping_for_update = "true"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
