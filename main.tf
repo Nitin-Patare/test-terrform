5042fc01e7e4712d2048874302ab4981e3f93e8e
Nitin-Patare <166031025+Nitin-Patare@users.noreply.github.com>
2024-04-04 20:01:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("terraform-key.json")
+   credentials = file("/root/terraform-key.json")
   project     = "prj-21-417004"
   region      = "us-central1"  # Set your desired region
 }
